9ab8b323e2e82ea83f9665da526a1928dbf827b5
Sandhiyakarthik <120768901+Sandhiyakarthik@users.noreply.github.com>
2023-02-21 11:34:52 +0530

    Add files via upload

---
+++ positive numbers in list.py
@@ -0,0 +1,14 @@
+def print_positive_numbers(numbers):
+    for num in numbers:
+        if num > 0:
+            print(num)
+
+# Example usage:
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+
+print("Positive numbers in list1:")
+print_positive_numbers(list1)
+
+print("Positive numbers in list2:")
+print_positive_numbers(list2)
